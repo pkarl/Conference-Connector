f0c2dafbc689fce21c64262a3aaa50a43a0aedbe
Pete Karl II <pete@bocoup.com>
2011-09-29 13:23:47 -0400

    hello worlding

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
-  console.log('Ready');
+var http = require('http');
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.log(err); }
-      process.setuid(stats.uid);
-    });
-  }
-});
+app.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:1337/');
+
+// app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+//   console.log('Ready');
+// 
+//   // if run as root, downgrade to the owner of this file
+//   if (process.getuid() === 0) {
+//     require('fs').stat(__filename, function(err, stats) {
+//       if (err) { return console.log(err); }
+//       process.setuid(stats.uid);
+//     });
+//   }
+// });
